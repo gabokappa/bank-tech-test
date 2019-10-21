21009b7cb6204a2e9f40e1f3c3131e23cf328eec
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-21 11:47:06 +0100

    Light refactoring

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,10 @@
 class Account
 attr_reader :balance
 
+  def initialize
+    @balance = 0
+    @activity = []
+  end
 
   def deposit(ammount)
     @balance = ammount
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -22,6 +22,4 @@ describe Account do
     end
   end
 
-
-
 end
