14f0a189d067f4e014394bf9f35a4699632dd5f9
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-21 11:25:35 +0100

    Second test passing green testing for withdraw functionality

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,13 @@
 class Account
 attr_reader :balance
 
+
   def deposit(ammount)
     @balance = ammount
+  end
+
+  def withdraw(ammount)
+    @balance =- ammount
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,12 @@ describe Account do
       expect(account.balance).to eq(10)
     end
   end
+
+  describe "#withdraw" do
+    it "takes funds away from the balance" do
+      account.withdraw(10)
+      expect(account.balance).to eq(-10)
+    end
+  end
+
 end
