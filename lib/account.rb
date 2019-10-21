b38541fb12742267e3ced743f061de74db79366c
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-21 12:22:11 +0100

    Activity is stored when a deposit is made

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,19 @@
 class Account
-attr_reader :balance
+attr_reader :balance, :activity
 
   def initialize
     @balance = 0
     @activity = []
   end
 
-  def deposit(ammount)
-    @balance = ammount
+  def deposit(amount)
+    t = Time.now.strftime('%d/%m/%Y')
+    @balance += amount.to_f
+    @activity.push("#{t} || #{sprintf("%.2f", amount)} || || #{sprintf("%.2f", @balance)}")
   end
 
-  def withdraw(ammount)
-    @balance =- ammount
+  def withdraw(amount)
+    @balance =- amount
   end
 
   def print_statement
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,22 +2,27 @@ require 'account'
 describe Account do
   subject(:account) { described_class.new }
 
-  describe "#deposit" do
-    it "deposits money into the balance" do
+  describe '#deposit' do
+    it 'deposits money into the balance' do
       account.deposit(10)
       expect(account.balance).to eq(10)
     end
+
+    it 'stores the timestamp of the deposit' do
+      account.deposit(10)
+      expect(account.activity).to eq ["21/10/2019 || 10.00 || || 10.00"]
+    end
   end
 
-  describe "#withdraw" do
-    it "takes funds away from the balance" do
+  describe '#withdraw' do
+    it 'takes funds away from the balance' do
       account.withdraw(10)
       expect(account.balance).to eq(-10)
     end
   end
 
-  describe "#print_statement" do
-    it "prints out account statement in a specific format" do
+  describe '#print_statement' do
+    it 'prints out account statement in a specific format' do
       expect { account.print_statement }. to output("date || credit || debit || balance\n").to_stdout
     end
   end
