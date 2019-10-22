bb92a322046130276228cfdb96b5fce1bedeb8fc
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-22 10:17:00 +0100

    Variable names changed in print_statement_spec

--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -14,15 +14,15 @@ describe ' #print_statement' do
     it 'prints out a statement with a header and event' do
       account.deposit(100)
       deposit = "#{@date} || 100.00 || || 100.00\n"
-      expect { account.print_statement }. to output("#{@header}#{deposit}").to_stdout
+      expect { account.print_statement }. to output(@header + deposit).to_stdout
     end
 
     it 'prints out a statement with the most recent event at the top' do
       account.deposit(100)
       account.withdraw(10)
-      deposit = "#{@date} || || 10.00 || 90.00\n"
-      withdraw = "#{@date} || 100.00 || || 100.00\n"
-      expect { account.print_statement }. to output(@header + deposit + withdraw).to_stdout
+      deposit = "#{@date} || 100.00 || || 100.00\n"
+      withdraw = "#{@date} || || 10.00 || 90.00\n"
+      expect { account.print_statement }. to output(@header + withdraw + deposit).to_stdout
     end
   end
 end
